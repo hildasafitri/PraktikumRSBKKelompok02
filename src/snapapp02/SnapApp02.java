cb303d550c8a3ebc05c2df6ec7f49b2865f72c7e
hildasafitri <hildansafitri@gmail.com>
2018-10-29 08:01:59 +0700

    upload pertama

---
+++ src/snapapp02/SnapApp02.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp02;
+
+/**
+ *
+ * @author Hilda
+ */
+public class SnapApp02 extends SnapFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapApp02 snap = new SnapApp02();
+        snap.setVisible(true);
+    }
+    
+}
